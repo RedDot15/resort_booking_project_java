4783630e7c768826f91d8acc43aea9bd9999dc1c
NITRO-AN515-56\Nam <namyahoo2003@gmail.com>
2023-10-22 14:38:48 +0700

    add front-end web

--- src/main/java/t3h/bigproject/controller/HomeController.java
+++ src/main/java/t3h/bigproject/controller/HomeController.java
@@ -4,17 +4,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
     @GetMapping(value = {"home","/"})
     public String home(Model model){
-        return "index.html";
+        return "frontend/index.html";
+    }
+
+    @RequestMapping("/city")
+    public String city(Model model){
+        return "frontend/city.html";
+    }
+
+    @RequestMapping("/resort")
+    public String resort(Model model){
+        return "frontend/resort.html";
+    }
+
+    @RequestMapping("/booking")
+    public String booking(Model model){
+        return "frontend/booking.html";
+    }
+
+    @RequestMapping("/thankyou")
+    public String thankyou(Model model){
+        return "frontend/thankyou.html";
     }
 
     @GetMapping(value = {"login"})
     public String login(Model model) {
-        return "login.html";
+        return "frontend/login.html";
     }
 
     @RequestMapping("/403")
